06b147f93c586d793b94fe6b93414b32404171a2
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-26 23:11:26 -0500

    begin cheatsheet

---
+++ a.js
@@ -0,0 +1,36 @@
+// use 'strict'
+
+var ohm = require ('ohm-js');
+
+let text = 'a';
+
+let grammar = Str.raw`
+simplelexicalgrammar {
+  main = "a"
+}
+`;
+
+let syntacticgrammar = Str.raw`
+simplesyntacticgrammar {
+  Main = "a"
+}
+
+let semanticsFunctions = {
+    main: function (capture) { return '*'; },
+    _iter: function (...children) { return children.map(c => c._glue ()); },
+    _terminal: function () { return this.sourceString; }
+};
+
+function parse (text) {
+    let parser = ohm.grammar (grammar);
+    let cst = parser.match (text);
+    if (cst.succeeded) {
+	let sem = parser.createSemantics ();
+	sem.addOperation ('op', semanticsFunctions);
+	console.log ( sem (cst)['op'] );
+    } else {
+	throw "something is terribly wrong";
+    }
+}
+
+parse (text);
