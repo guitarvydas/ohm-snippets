b6f5730b608d66a577c051d94aaffd089e235b9f
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-26 22:37:29 -0500

    prule

--- dev.bash
+++ dev.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 cwd=`pwd`
-pfr - object-initializer.ohm object-initializer.glue $cwd/support.js
+#pfr - object-initializer.ohm object-initializer.glue $cwd/support.js
+pre '.' '$' prule.ohm prule.glue --stop=1 --input=prule.txt
+pre '.' '$' prule2.ohm prule2.glue --stop=1 --input=prule.txt
